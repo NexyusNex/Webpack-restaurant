fdd834796c92f379f376224b539d7dd773711373
NexyusNex <shootdietale@gmail.com>
2023-01-14 14:24:02 +0100

    Webpack compiles fine

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    bundle: path.resolve(__dirname, ".src/index.js"),
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, ".dist/index.html"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
   },
 };
