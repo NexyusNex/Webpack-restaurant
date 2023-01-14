938c43e24236efad9525d68ea724b60c8267aefb
NexyusNex <shootdietale@gmail.com>
2023-01-14 15:15:38 +0100

    The content image and text now loads from a function as well

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function pageLoad() {
   const body = document.querySelector("body");
+  //header
   const header = document.createElement("div");
   header.classList.add("header");
 
@@ -16,6 +17,30 @@ function pageLoad() {
   header.appendChild(h1);
   header.appendChild(ul);
   body.appendChild(header);
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+  const desc = document.createElement("div");
+  desc.classList.add("description");
+
+  const img = document.createElement("img");
+  img.setAttribute("alt", "food");
+  img.setAttribute("src", "../food.jpg");
+  const text = document.createElement("div");
+  text.textContent =
+    "We are a tight-knit, fun-loving, devoted team of local cooks spreading" +
+    "the gospel of good times and good food. We offer limited capacity onsite" +
+    "events in our restaurant kitchen space. And worry not, our krewe will" +
+    "travel to your destination of choice from hotel ballrooms to private" +
+    "kitchens to entertain groups of all sizes. We cook, we tell stories, we" +
+    " deliver informative culinary demonstrations and lectures, but most of" +
+    "all we treat every event like you're a guest at our dinner table. Join" +
+    "us!";
+
+  desc.appendChild(img);
+  desc.appendChild(text);
+  container.appendChild(desc);
+  body.appendChild(container);
 }
 
 pageLoad();
