b7e704c1d484279d9b2f736ea1c16dedcdf384e0
NexyusNex <shootdietale@gmail.com>
2023-01-14 15:42:55 +0100

    Pageload is now in its own file and inside a module which we call in index.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("function pageLoad() {\r\n  const body = document.querySelector(\"body\");\r\n  //header\r\n  const header = document.createElement(\"div\");\r\n  header.classList.add(\"header\");\r\n\r\n  const h1 = document.createElement(\"h1\");\r\n  h1.textContent = \"Restaurant page\";\r\n\r\n  const ul = document.createElement(\"ul\");\r\n  const list = [\"Home\", \"Contact\", \"Menu\"];\r\n  for (let i = 0; i < 3; i++) {\r\n    const li = document.createElement(\"li\");\r\n    li.textContent = list[i];\r\n    ul.appendChild(li);\r\n  }\r\n  header.appendChild(h1);\r\n  header.appendChild(ul);\r\n  body.appendChild(header);\r\n\r\n  const container = document.createElement(\"div\");\r\n  container.classList.add(\"container\");\r\n  const desc = document.createElement(\"div\");\r\n  desc.classList.add(\"description\");\r\n\r\n  const img = document.createElement(\"img\");\r\n  img.setAttribute(\"alt\", \"food\");\r\n  img.setAttribute(\"src\", \"../food.jpg\");\r\n  const text = document.createElement(\"div\");\r\n  text.textContent =\r\n    \"We are a tight-knit, fun-loving, devoted team of local cooks spreading\" +\r\n    \"the gospel of good times and good food. We offer limited capacity onsite\" +\r\n    \"events in our restaurant kitchen space. And worry not, our krewe will\" +\r\n    \"travel to your destination of choice from hotel ballrooms to private\" +\r\n    \"kitchens to entertain groups of all sizes. We cook, we tell stories, we\" +\r\n    \" deliver informative culinary demonstrations and lectures, but most of\" +\r\n    \"all we treat every event like you're a guest at our dinner table. Join\" +\r\n    \"us!\";\r\n\r\n  desc.appendChild(img);\r\n  desc.appendChild(text);\r\n  container.appendChild(desc);\r\n  body.appendChild(container);\r\n}\r\n\r\npageLoad();\r\n\n\n//# sourceURL=webpack://webpack-restaurant/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pageload__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pageload */ \"./src/pageload.js\");\n\r\n\r\n(0,_pageload__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\n\n//# sourceURL=webpack://webpack-restaurant/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/pageload.js":
+/*!*************************!*\
+  !*** ./src/pageload.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction pageLoad() {\r\n  const body = document.querySelector(\"body\");\r\n  //header\r\n  const header = document.createElement(\"div\");\r\n  header.classList.add(\"header\");\r\n\r\n  const h1 = document.createElement(\"h1\");\r\n  h1.textContent = \"Restaurant page\";\r\n\r\n  const ul = document.createElement(\"ul\");\r\n  const list = [\"Home\", \"Contact\", \"Menu\"];\r\n  for (let i = 0; i < 3; i++) {\r\n    const li = document.createElement(\"li\");\r\n    li.textContent = list[i];\r\n    ul.appendChild(li);\r\n  }\r\n  header.appendChild(h1);\r\n  header.appendChild(ul);\r\n  body.appendChild(header);\r\n\r\n  const container = document.createElement(\"div\");\r\n  container.classList.add(\"container\");\r\n  const desc = document.createElement(\"div\");\r\n  desc.classList.add(\"description\");\r\n\r\n  const img = document.createElement(\"img\");\r\n  img.setAttribute(\"alt\", \"food\");\r\n  img.setAttribute(\"src\", \"../food.jpg\");\r\n  const text = document.createElement(\"div\");\r\n  text.textContent =\r\n    \"We are a tight-knit, fun-loving, devoted team of local cooks spreading\" +\r\n    \"the gospel of good times and good food. We offer limited capacity onsite\" +\r\n    \"events in our restaurant kitchen space. And worry not, our krewe will\" +\r\n    \"travel to your destination of choice from hotel ballrooms to private\" +\r\n    \"kitchens to entertain groups of all sizes. We cook, we tell stories, we\" +\r\n    \" deliver informative culinary demonstrations and lectures, but most of\" +\r\n    \"all we treat every event like you're a guest at our dinner table. Join\" +\r\n    \"us!\";\r\n\r\n  desc.appendChild(img);\r\n  desc.appendChild(text);\r\n  container.appendChild(desc);\r\n  body.appendChild(container);\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (pageLoad());\r\n\n\n//# sourceURL=webpack://webpack-restaurant/./src/pageload.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
